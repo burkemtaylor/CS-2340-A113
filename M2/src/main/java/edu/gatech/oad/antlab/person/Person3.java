ea1ed3ed5c3ad8276def225175a5c9365a2165b2
gecage952 <gcage6@gatech.edu>
2017-09-07 19:47:20 -0400

    Added my Name in the javadoc

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person3.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person3.java
@@ -4,7 +4,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  reversed string
  *
- *  @author  Bob
+ *  @author  Gregory Cage
  *  @version 1.1
  */
 public class Person3 {
