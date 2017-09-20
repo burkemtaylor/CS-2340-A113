5e0d6d4a4da0da53da83c86a95b3953523f51726
Neeraj Sabapathy <Neeraj@lawn-128-61-60-189.lawn.gatech.edu>
2017-09-19 20:36:48 -0400

    Model/User.java

---
+++ MyApplication/app/src/main/java/com/example/burketaylor/rattracker/Model/User.java
@@ -0,0 +1,28 @@
+package com.example.burketaylor.rattracker.User;
+
+/**
+ * Created by Neeraj on 9/19/17.
+ */
+
+public class User {
+    private String userName;
+    private String password;
+    private boolean isLocked;
+    public User(String uN, String p, boolean x) {
+        userName = uN;
+        password = p;
+        isLocked = x;
+    }
+    public User() {
+        userName = "user";
+        password = "pass";
+        isLocked = false;
+    }
+
+    public boolean logIn(String user, String pass) {
+        if (userName.equals(user) && password.equals(pass)) {
+            return true;
+        }
+        return false;
+    }
+}
