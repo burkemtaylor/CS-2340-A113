1b0b0c00b98caec0ce25b71acbd76dd8af736c5d
gecage952 <gcage6@gatech.edu>
2017-09-26 19:59:49 -0400

    created database class

---
+++ MyApplication/app/src/main/java/com/example/burketaylor/rattracker/model/Database.java
@@ -0,0 +1,37 @@
+package com.example.burketaylor.rattracker.model;
+
+/**
+ * Created by gecag on 9/26/2017.
+ */
+
+import java.util.ArrayList;
+
+public class Database {
+
+
+    ArrayList<User> userBase = new ArrayList<>();
+    ArrayList<User> adminBase = new ArrayList<>();
+
+
+    public static void addUser(User user) {
+
+    }
+
+
+    public static void addAdmin(User user) {
+
+    }
+
+    public static User getUser() {
+
+        return null;
+
+    }
+
+    public static User getAdmin() {
+
+        return null;
+
+    }
+
+}
