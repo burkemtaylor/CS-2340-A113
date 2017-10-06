f8c87480042f80691d6c9591c763bdb522043c26
gecage952 <gcage6@gatech.edu>
2017-10-05 21:35:03 -0400

    Implementing Async task

--- MyApplication/app/src/main/java/com/example/burketaylor/rattracker/controller/ListActivity.java
+++ MyApplication/app/src/main/java/com/example/burketaylor/rattracker/controller/ListActivity.java
@@ -1,6 +1,7 @@
 package com.example.burketaylor.rattracker.controller;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -16,8 +17,12 @@ import com.example.burketaylor.rattracker.model.RatSighting;
 import com.example.burketaylor.rattracker.model.RatSightingDatabase;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 public class ListActivity extends AppCompatActivity {
+
+    ListView listView;
+    String[] mobileArray;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,23 +31,12 @@ public class ListActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        try {
-            new RatSightingDatabase(this.getResources().openRawResource(R.raw.rat_sightings));
-        } catch (IOException e) {
-            Log.d("Scan error", e.getLocalizedMessage());
-        }
 
-        Object[] ratArray = RatSightingDatabase.getMap().values().toArray();
-        String[] mobileArray = new String[RatSightingDatabase.getMap().size()];
-        for (int i = 0; i < mobileArray.length; i++) {
-            mobileArray[i] = i + 1 + ". " + ((RatSighting) ratArray[i]).getUniqueKey();
-        }
 
-        ArrayAdapter adapter = new ArrayAdapter<String>(this,
-               android.R.layout.simple_list_item_1, mobileArray);
 
-        ListView listView = (ListView) findViewById(R.id.mobile_list);
-        listView.setAdapter(adapter);
+
+        listView = (ListView) findViewById(R.id.mobile_list);
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -52,6 +46,8 @@ public class ListActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        new ScanTask().execute();
 
 
     }
@@ -63,5 +59,35 @@ public class ListActivity extends AppCompatActivity {
     public void logout(View view) {
         Intent intent = new Intent(this, com.example.burketaylor.rattracker.controller.WelcomeActivity.class);
         startActivity(intent);
+    }
+
+    private class ScanTask extends AsyncTask<InputStream, Void, Void> {
+
+        protected Void doInBackground(InputStream... in) {
+            try {
+                new RatSightingDatabase(ListActivity.this.getResources().openRawResource(R.raw.Rat_Sightings));
+            } catch (IOException e) {
+                Log.d("Scan error", e.getLocalizedMessage());
+            }
+
+            Object[] ratArray = RatSightingDatabase.getMap().values().toArray();
+            String[] mobileArray = new String[RatSightingDatabase.getMap().size()];
+            for (int i = 0; i < mobileArray.length; i++) {
+                mobileArray[i] = i + 1 + ". " + ((RatSighting) ratArray[i]).getUniqueKey();
+            }
+
+
+            return null;
+        }
+
+
+        protected void onPostExecute(Void v) {
+            ArrayAdapter adapter = new ArrayAdapter<String>(ListActivity.this,
+                    android.R.layout.simple_list_item_1, mobileArray);
+
+
+            listView.setAdapter(adapter);
+
+        }
     }
 }
