9f6f75cf2c8218988b4498873d392a5b0cfc2bc7
alanh678 <alanh678@gmail.com>
2017-10-15 20:38:57 -0400

    Formatting fixes for RatSightingDatabase

--- MyApplication/app/src/main/java/com/example/burketaylor/rattracker/model/RatSightingDatabase.java
+++ MyApplication/app/src/main/java/com/example/burketaylor/rattracker/model/RatSightingDatabase.java
@@ -11,7 +11,7 @@ import java.util.HashMap;
  */
 
 public class RatSightingDatabase {
-    private static HashMap<String, RatSighting> ratSightingMap= new HashMap<>();
+    private static HashMap<String, RatSighting> ratSightingMap = new HashMap<>();
     private static String lastSelected = null;
 
     /**
