bea6298b6520d65dfb2405869c8ab5c0eb754b74
burketaylor <burkemtaylor@gmail.com>
2017-11-14 20:32:50 -0500

    set up testing imports

--- MyApplication/app/src/main/java/com/example/burketaylor/rattracker/model/JUnitTests.java
+++ MyApplication/app/src/main/java/com/example/burketaylor/rattracker/model/JUnitTests.java
@@ -4,5 +4,32 @@ package com.example.burketaylor.rattracker.model;
  * Created by burketaylor on 11/14/17.
  */
 
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.testng.annotations.Test;
+
 public class JUnitTests {
+    @Before
+
+    @Test
+    public void test1() {
+
+    }
+
+    @Test
+    public void test2() {
+
+    }
+
+    @Test
+    public void test3() {
+
+    }
+
+    @Test
+    public void test4() {
+
+    }
 }
