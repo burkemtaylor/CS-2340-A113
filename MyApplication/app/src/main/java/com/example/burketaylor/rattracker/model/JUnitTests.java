301a45874088d7e43ee381b9cd0440d7a6ae2eb1
alanh678 <alanh678@gmail.com>
2017-11-14 21:11:42 -0500

    Finished testUserCompareTo

--- MyApplication/app/src/main/java/com/example/burketaylor/rattracker/model/JUnitTests.java
+++ MyApplication/app/src/main/java/com/example/burketaylor/rattracker/model/JUnitTests.java
@@ -18,8 +18,22 @@ public class JUnitTests {
 
     }
 
+    /**
+     * Tests compareTo method for User class
+     * Made by Alan Hoang, 11/14/2017
+     */
     @Test
     public void testUserCompareTo() {
+        User user1 = new User("joseph", "password");
+        User user2 = new User("joseph", "password");
+        User user3 = new User("robert","differentpassword");
+        User user4 = new User("joseph", "differentpassword");
+
+        Assert.assertEquals("Same username & password", 0, user1.compareTo(user2));
+        Assert.assertEquals("Different username & password", -1, user1.compareTo(user3));
+        Assert.assertEquals("Same username, different password", 0, user1.compareTo(user4));
+        Assert.assertEquals("Different username, same password", -1, user3.compareTo(user4));
+        Assert.assertEquals("Comparing with null", -1, user1.compareTo(null));
     }
 
     @Test
